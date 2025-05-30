e65ba1c517f4c44fb164f80639b97dedd56e8b0c
paradoquiso <44910940+paradoquiso@users.noreply.github.com>
2025-05-30 09:59:37 -0300

    Update produto.py

--- src/models/produto.py
+++ src/models/produto.py
@@ -18,9 +18,10 @@ class Produto(db.Model):
     enviado = db.Column(db.Integer, default=0)  # 0 = não enviado, 1 = enviado
     data_envio = db.Column(db.String(50))
     
-    # Campos para autenticação PI
+    # Campos para autenticação PI - Modificados para maior segurança
     responsavel_pi = db.Column(db.String(100))  # Nome do responsável pela senha PI
-    senha_pi = db.Column(db.String(10))  # Senha PI utilizada (para auditoria)
+    senha_pi_hash = db.Column(db.String(200))  # Hash da senha PI (substituindo senha_pi)
+    data_autorizacao = db.Column(db.String(50))  # Data/hora da autorização
     
     # Campos para validação administrativa
     validado = db.Column(db.Integer, default=0)  # 0 = não validado, 1 = validado
@@ -41,7 +42,7 @@ class Produto(db.Model):
             'enviado': self.enviado,
             'data_envio': self.data_envio,
             'responsavel_pi': self.responsavel_pi,
-            'senha_pi': self.senha_pi,
+            # Removido o campo senha_pi para não expor em APIs ou interfaces
             'validado': self.validado,
             'validador_id': self.validador_id,
             'data_validacao': self.data_validacao
